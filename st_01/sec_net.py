41a88ad5d49509acb514be3bfe6a5cb5f35408ed
Ting Sun <tsun@connect.ust.hk>
2018-12-31 16:15:55 +0800

    add st_01/train_w_seed_constrain.py

--- st_01/sec_net.py
+++ st_01/sec_net.py
@@ -101,6 +101,22 @@ class SeedingLoss(nn.Module):
         loss = -(cues * sm_mask.log()).sum()/count
         return loss
 
+#----------------------------------------------------------------------------------------------------------------
+def crf(sm_mask_one, img_one, num_class, input_size, mask_size, num_iter):
+    sm_u = np.transpose(resize(np.transpose(sm_mask_one, [1,2,0]), input_size, mode='constant'),[2,0,1])
+
+    U = unary_from_softmax(sm_u)
+
+    d = dcrf.DenseCRF2D(input_size[0], input_size[1], num_class)
+    d.setUnaryEnergy(U)
+
+    d.addPairwiseGaussian(sxy=(3,3), compat=3, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+    d.addPairwiseBilateral(sxy=(30,30), srgb=(13,13,13), rgbim=img_one.astype(np.uint8), compat=20, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+
+    Q = d.inference(num_iter)
+    return np.transpose(resize(np.transpose(np.array(Q).reshape((num_class, input_size[0], input_size[1])),[1,2,0]), mask_size, mode='constant'), [2,0,1])
+
+
 
 class CRFLayer():
     def __init__(self, flag_multi_process = False):
@@ -108,18 +124,18 @@ class CRFLayer():
         self.min_prob = 0.0001
         self.mask_size = [41, 41]
         self.input_size = [321, 321]
-        self.nun_iter = 5
+        self.num_iter = 5
         if flag_multi_process:
             num_cores = os.cpu_count()
             self.pool = Pool(processes=num_cores)
 
 
-    def run(self, sm_mask, img, flag_train): # the input array are detached numpy already
+    def run(self, sm_mask, img): # the input array are detached numpy already
         batch_size = sm_mask.shape[0]
         result = np.zeros(sm_mask.shape)
         for i in range(batch_size):
 
-            sm_u = np.transpose(resize(np.transpose(sm_mask[i], [1,2,0]), self.input_size, mode='constant'),[3,0,1])
+            sm_u = np.transpose(resize(np.transpose(sm_mask[i], [1,2,0]), self.input_size, mode='constant'),[2,0,1])
 
             U = unary_from_softmax(sm_u)
 
@@ -129,35 +145,46 @@ class CRFLayer():
             d.addPairwiseGaussian(sxy=(3,3), compat=3, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
             d.addPairwiseBilateral(sxy=(30,30), srgb=(13,13,13), rgbim=img[i].astype(np.uint8), compat=20, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
 
-            Q = d.inference(self.nun_iter)
-            result[i] = Q
+            Q = d.inference(self.num_iter)
+            result[i] = np.transpose(resize(np.transpose(np.array(Q).reshape((self.num_class, self.input_size[0], self.input_size[1])),[1,2,0]), self.mask_size, mode='constant'), [2,0,1])
 
         return result
 
     # for parallel running ------------------------------
-    def run_parallel(self, sm_mask, img, flag_train): # flag_train is for the strange dif between train & test in org SEC
+    def crf(self, sm_mask_one, img_one):
+        sm_u = np.transpose(resize(np.transpose(sm_mask_one, [1,2,0]), self.input_size, mode='constant'),[2,0,1])
+
+        U = unary_from_softmax(sm_u)
+
+        d = dcrf.DenseCRF2D(self.input_size[0], self.input_size[1], self.num_class)
+        d.setUnaryEnergy(U)
+
+        d.addPairwiseGaussian(sxy=(3,3), compat=3, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+        d.addPairwiseBilateral(sxy=(30,30), srgb=(13,13,13), rgbim=img_one.astype(np.uint8), compat=20, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+
+        Q = d.inference(self.nun_iter)
+        return np.transpose(resize(np.transpose(np.array(Q).reshape((self.num_class, self.input_size[0], self.input_size[1])),[1,2,0]), self.mask_size, mode='constant'), [2,0,1])
+
+
+    def run_parallel(self, sm_mask, img): # flag_train is for the strange dif between train & test in org SEC
         batch_size = sm_mask.shape[0]
-        unary = np.transpose(sm_mask, [0, 2, 3, 1])
+        result = np.zeros(sm_mask.shape)
 
-        if flag_train:
-            result = np.zeros(unary.shape)
+        # temp = self.pool.starmap(self.crf,[(sm_mask[i], img[i]) for i in range(batch_size)])
+        temp = self.pool.starmap(crf,[(sm_mask[i], img[i], self.num_class, self.input_size, self.mask_size, self.num_iter) for i in range(batch_size)])
+        for i in range(batch_size):
+            result[i] = temp[i]
 
-            temp = self.pool.starmap(CRF,[(resize(img[i]/255.0, self.mask_size, mode='constant')*255, unary[i], 10, 12.0) for i in range(batch_size)])
-            for i in range(batch_size):
-                result[i] = temp[i]
+        return result
 
-            result = np.transpose(result, [0, 3, 1, 2])
-            result[result < self.min_prob] = self.min_prob
-            result = result / np.sum(result, axis=1, keepdims=True)
 
-            return np.log(result)
+class ConstrainLossLayer(nn.Module):
+    def __init__(self):
+        super(ConstrainLossLayer, self).__init__()
+        self.num_pixel = 41 * 41
 
-        else:
-            result = np.zeros([batch_size, self.input_size[0], self.input_size[1]])
-            unary[unary < self.min_prob] = self.min_prob
-
-            temp = self.pool.starmap(CRF,[(img[i], np.log(resize(unary[i], self.input_size, mode='constant')), 10, 1.0) for i in range(batch_size)])
-            for i in range(batch_size):
-                result[i, :, :] = np.argmax(temp[i], axis=2)
-
-            return result
+    def forward(self, crf_sm_mask, sm_mask, flag_use_cuda):
+        temp = torch.from_numpy(crf_sm_mask.astype('float32'))
+        if flag_use_cuda:
+            temp = temp.cuda()
+        return ((temp * (temp/sm_mask).log()).sum()/self.num_pixel)/sm_mask.shape[0]
