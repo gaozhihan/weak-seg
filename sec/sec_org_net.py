86e1d0d6c885887510b4ce73e6ca94873babdbe4
Ting Sun <tsun@connect.ust.hk>
2019-01-02 15:09:45 +0800

    add sec/train_sec_st01.py, make the crf and constrain loss same as that in st_01

--- sec/sec_org_net.py
+++ sec/sec_org_net.py
@@ -8,6 +8,9 @@ from joblib import Parallel, delayed
 from multiprocessing import Pool
 import os
 
+import pydensecrf.densecrf as dcrf
+from pydensecrf.utils import unary_from_softmax, create_pairwise_bilateral
+
 class SEC_NN(nn.Module):
     def __init__(self):
         super(SEC_NN, self).__init__()
@@ -216,3 +219,77 @@ class ExpandLossLayer(nn.Module):
         return loss/batch_size
 
 # --- my modified part -----------------------------------------------
+def crf(sm_mask_one, img_one, num_class, input_size, mask_size, num_iter):
+    sm_u = np.transpose(resize(np.transpose(sm_mask_one, [1,2,0]), input_size, mode='constant'),[2,0,1])
+
+    U = unary_from_softmax(sm_u)
+
+    d = dcrf.DenseCRF2D(input_size[0], input_size[1], num_class)
+    d.setUnaryEnergy(U)
+
+    d.addPairwiseGaussian(sxy=(3,3), compat=3, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+    d.addPairwiseBilateral(sxy=(30,30), srgb=(13,13,13), rgbim=img_one.astype(np.uint8), compat=20, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+
+    Q = d.inference(num_iter)
+    return np.array(Q).reshape((num_class, input_size[0], input_size[1]))
+
+
+
+class STCRFLayer():
+    def __init__(self, flag_multi_process = False):
+        self.num_class = 21
+        self.min_prob = 0.0001
+        self.mask_size = [41, 41]
+        self.input_size = [321, 321]
+        self.num_iter = 5
+        if flag_multi_process:
+            num_cores = os.cpu_count()
+            self.pool = Pool(processes=num_cores)
+
+
+    def run(self, sm_mask, img): # the input array are detached numpy already
+        batch_size = sm_mask.shape[0]
+        result_big = np.zeros((sm_mask.shape[0], sm_mask.shape[1], self.input_size[0], self.input_size[1]))
+        result_small = np.zeros(sm_mask.shape)
+        for i in range(batch_size):
+
+            sm_u = np.transpose(resize(np.transpose(sm_mask[i], [1,2,0]), self.input_size, mode='constant'),[2,0,1])
+
+            U = unary_from_softmax(sm_u)
+
+            d = dcrf.DenseCRF2D(self.input_size[0], self.input_size[1], self.num_class)
+            d.setUnaryEnergy(U)
+
+            d.addPairwiseGaussian(sxy=(3,3), compat=3, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+            d.addPairwiseBilateral(sxy=(30,30), srgb=(13,13,13), rgbim=img[i].astype(np.uint8), compat=20, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC)
+
+            Q = d.inference(self.num_iter)
+            result_big[i] = np.array(Q).reshape((self.num_class, self.input_size[0], self.input_size[1]))
+            result_small[i] = np.transpose(resize(np.transpose(result_big[i],[1,2,0]), self.mask_size, mode='constant'), [2,0,1])
+
+        return result_big, result_small
+
+    def run_parallel(self, sm_mask, img): # flag_train is for the strange dif between train & test in org SEC
+        batch_size = sm_mask.shape[0]
+        result_big = np.zeros((sm_mask.shape[0], sm_mask.shape[1], self.input_size[0], self.input_size[1]))
+        result_small = np.zeros(sm_mask.shape)
+
+        # temp = self.pool.starmap(self.crf,[(sm_mask[i], img[i]) for i in range(batch_size)])
+        temp = self.pool.starmap(crf,[(sm_mask[i], img[i], self.num_class, self.input_size, self.mask_size, self.num_iter) for i in range(batch_size)])
+        for i in range(batch_size):
+            result_big[i] = temp[i]
+            result_small[i] = np.transpose(resize(np.transpose(result_big[i],[1,2,0]), self.mask_size, mode='constant'), [2,0,1])
+
+        return result_big, result_small
+
+
+class STConstrainLossLayer(nn.Module):
+    def __init__(self):
+        super(STConstrainLossLayer, self).__init__()
+        self.num_pixel = 41 * 41
+
+    def forward(self, crf_sm_mask, sm_mask, flag_use_cuda):
+        temp = torch.from_numpy(crf_sm_mask.astype('float32'))
+        if flag_use_cuda:
+            temp = temp.cuda()
+        return ((temp * (temp/sm_mask).log()).sum()/self.num_pixel)/sm_mask.shape[0]
