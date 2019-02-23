83ca04bf013f92039427a59f2c2070b6bd7dced1
tsun <tsun@ust.hk>
2019-02-23 13:29:57 +0800

    local commit

--- st_resnet/train_sec_st_01_multi_scale_flip.py
+++ st_resnet/train_sec_st_01_multi_scale_flip.py
@@ -256,7 +256,7 @@ for epoch in range(args.epochs):
 
     if eval_iou.mean() > max_iou:
         print('save model ' + args.model + ' with val mean iou: {}'.format(eval_iou.mean()))
-        torch.save(net.state_dict(), './st_resnet/models/res_wsc_0222_'+ args.model + '.pth')
+        torch.save(net.state_dict(), './st_resnet/models/res_wsc_0223_'+ args.model + '.pth')
         max_iou = eval_iou.mean()
 
     # print('cur eval iou is : ', eval_iou, ' mean: ', eval_iou.mean())
