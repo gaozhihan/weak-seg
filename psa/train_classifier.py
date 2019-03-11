7f11e2331a708c5186d9d6adf50989580d71ec8e
Lei Tai <onlytailei@hotmail.com>
2019-03-11 15:24:54 +0800

    revise path

--- psa/train_classifier.py
+++ psa/train_classifier.py
@@ -40,6 +40,11 @@ elif host_name == 'ram-lab-server01':
     model_path = '/data_shared/Docker/tsun/docker/program/weak-seg/models/resnet50_feat.pth'
     # model_path = '/data_shared/Docker/tsun/docker/program/weak-seg/multi_scale/models/st_top_val_rec_my_resnet_9_9.pth'
     args.batch_size = 10
+elif host_name == 'weak_seg':
+    args.data_dir = '/home/VOC2012_SEG_AUG'
+    # model_path = '/data_shared/Docker/tsun/docker/program/weak-seg/models/resnet50_feat.pth'
+    # model_path = '/data_shared/Docker/tsun/docker/program/weak-seg/multi_scale/models/st_top_val_rec_my_resnet_9_9.pth'
+    args.batch_size = 10
 
 # net = st_resnet.resnet_st.resnet50(pretrained=False, num_classes=args.num_classes)
 # net = st_resnet.resnet_st_more_drp.resnet50(pretrained=False, num_classes=args.num_classes)
