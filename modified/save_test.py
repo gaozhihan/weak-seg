6aab7c437ff70bcbb83a41b69f79def04e967e8b
gaozhihan <gaozhihan@users.noreply.github.com>
2019-03-19 22:05:40 +0800

    change the format for saving test results

--- modified/save_test.py
+++ modified/save_test.py
@@ -55,7 +55,7 @@ class SaveTest(object):
         if data_dir is None:
             data_dir = "/home/data/gaozhihan/project/weak-seg/weak-seg-aux/VOC2012_SEG_AUG"
         if save_dir is None:
-            save_dir = '/home/data/gaozhihan/project/weak-seg/weak-seg-aux/results/VOC2012/Segmentation/comp5_test_cls'
+            save_dir = '/home/data/gaozhihan/project/weak-seg/weak-seg-aux/results/VOC2012/Segmentation/comp6_test_cls'
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         self.data_dir = data_dir
@@ -73,18 +73,15 @@ class SaveTest(object):
     def _counter_max(self):
         return len(self.file_list)
 
-    def _get_save_path(self):
-        ret = os.path.join(self.save_dir, self.file_list[self.counter] + '.png')
-        return ret
-
     def _get_orig_img_path(self):
         ret = os.path.join(self.data_dir, "images", self.file_list[self.counter]+".png")
         return ret
 
-    def _get_save_shape(self):
+    def _get_save_info(self):
+        save_path = os.path.join(self.save_dir, self.file_list[self.counter] + '.png')
         orig_img_path = self._get_orig_img_path()
         orig_img = Image.open(orig_img_path)
-        return orig_img.size
+        return save_path, orig_img.size
 
     def save_test(self, mask):
         '''
@@ -96,13 +93,23 @@ class SaveTest(object):
         '''
         if self.counter >= self._counter_max:
             raise ValueError('counter larger than the number of test files')
-        save_path = self._get_save_path()
-        save_shape = self._get_save_shape()
+        save_path, save_shape = \
+            self._get_save_info()
 
         img = mask.astype(np.uint8)
         img = toRGBarray(img, 21)
         img = Image.fromarray(img, mode='RGB')
         img = img.resize(size=save_shape)
+        palette_img = Image.new('P', (16, 16))
+        palette_img.putpalette(color_map(N=21))
+        # please refer to link https://stackoverflow.com/questions/29433243/convert-image-to-specific-palette-using-pil-without-dithering/29438149#29438149?newreg=5f7fd6d385d1455baefe9a1d3134cba2
+        # im.convert receive no keyword args. 'P' for mode and 0 for dither
+        # 3 args to create a new ImagingCore object
+        # mode (required): mode string (e.g. "P")
+        # dither (optional, default 0): PIL passes 0 or 1
+        # paletteimage (optional): An ImagingCore with a palette
+        img_im = img.im.convert('P', 0, palette_img.im)
+        img = img._new(img_im)
         img.save(save_path)
         self.counter += 1
         return self.counter
